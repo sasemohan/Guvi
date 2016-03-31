ae932ccc4204ca579001068d3d3d3cd8e0ef481c
Sasemohan <sasemohan90950@gmail.com>
2016-03-31 23:20:23 +0530

    Create Odd_Even.c

---
+++ Odd_Even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("entr the number ");
+scanf("%d",&a);
+if(a!=0)
+{
+if(a%2==0)
+printf("the number is even");
+else
+printf("even");
+}
+else
+{
+printf("zero");
+}
+}
