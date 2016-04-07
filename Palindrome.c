dbf7eaf21e206bf9aea8ad9c471f07205aab4ca1
Sasemohan <sasemohan90950@gmail.com>
2016-04-07 20:15:26 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,tem,res;
+printf("enter an randon numeric value");
+scanf("%d",&a);
+do{
+tem=a%10;
+tem=tem/10;
+res=res*10+tem;
+}while(tem!=0)
+if(num==res)
+{
+printf("palindrome");
+}
+}
