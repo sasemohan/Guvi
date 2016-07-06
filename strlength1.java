dca378e5889bb35a2ccb5959e5d59a0338aa596a
Sasemohan <sasemohan90950@gmail.com>
2016-07-06 12:16:40 +0530

    Rename strlength.java to strlength1.java

---
+++ strlength1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Test{
+   public static void main(String args[]){
+      String Str1 = new String("Welcome");
+    
+
+      System.out.print("String Length :" );
+      System.out.println(Str1.length());
+
+     }
+}
