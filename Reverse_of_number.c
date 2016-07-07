6461073d23d1b0e8f2b73e4b5b77b8c7cfb3ecd4
Sasemohan <sasemohan90950@gmail.com>
2016-07-08 00:33:25 +0530

    Update Reverse_of_number.c

--- Reverse_of_number.c
+++ Reverse_of_number.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
 int a=0,tem=0,res=0;
+clrscr();
 printf("enter an random numeric value");
 scanf("%d",&a);
 while(a!=0){
@@ -9,5 +11,6 @@ tem=a%10;
 res=res*10+tem;
 a=a/10;
 }
-printf("REVERSED %d",res)
+printf("REVERSED %d",res);
+getch();
 }
